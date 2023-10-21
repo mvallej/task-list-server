fc5650056c2aacf4d7cda61c340b1a605fd400bd
María Val <mvallej45@gmail.com>
2023-10-21 02:34:24 -0500

    About-it

--- app.js
+++ app.js
@@ -1,60 +1,22 @@
-require('dotenv').config();
 
 const express = require('express');
-const jwt = require('jsonwebtoken');
-
 const app = express();
+const tasksRouter = require('./routes/tasks');
+
+
 app.use(express.json());
 
-const users = [
-  {
-    username: 'usuario1',
-    password: 'password1',
-  },
-  {
-    username: 'usuario2',
-    password: 'password2',
-  },
-];
 
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
+app.use('/api', tasksRouter);
 
-  
-  const user = users.find((user) => user.username === username && user.password === password);
-
-  if (!user) {
-    return res.status(401).json({ message: 'Credenciales incorrectas' });
-  }
-
- 
-  const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET, {
-    expiresIn: '1h', 
-  });
-
-  res.json({ token });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor en funcionamiento en el puerto ${PORT}`);
 });
 
 
-app.get('/protegido', (req, res) => {
-  const token = req.headers.authorization;
 
-  if (!token) {
-    return res.status(401).json({ message: 'Token no proporcionado' });
-  }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ message: 'Token inválido' });
-    }
-
-    res.json({ message: 'Ruta protegida', user: decoded.username });
-  });
-});
-
-app.listen(3000, () => {
-  console.log('Servidor en funcionamiento en el puerto 3000');
-});
 
 
 
---
+++ review-3.md
@@ -0,0 +1,6 @@
+
+Quiero presentarles el producto de lista de tareas más eficiente y seguro. Te permitirá organizarte en el día a día agregando tus tareas pendientes, las cuales posteriormente puedes actualizar con funcionalidades como editar, marcar como completa o eliminar.
+
+Adicionalmente no tienes que preocuparte por la seguridad de tu información ya que se han implementado los más innovadores y seguros sistemas de autenticación y obtención de autorizaciones con lo cual no tendras problemas con que usuarios no autorizados puedan acceder a tus datos.
+
+Es una aplicación muy útil y de fácil manejo que agradecerás tener en el día a día.
