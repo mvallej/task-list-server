28dd3c29b4b4564549b570e893cd599ca2890ea5
María Val <mvallej45@gmail.com>
2023-10-21 01:29:51 -0500

    authentication

--- app.js
+++ app.js
@@ -1,29 +1,60 @@
+require('dotenv').config();
+
 const express = require('express');
+const jwt = require('jsonwebtoken');
+
 const app = express();
-const port = 3000;
-
-const { validateRequestMethod } = require('./middlewares'); 
-
-
-const listViewRouter = require('./list-view-router');
-const listEditRouter = require('./list-edit-router');
-
 app.use(express.json());
 
+const users = [
+  {
+    username: 'usuario1',
+    password: 'password1',
+  },
+  {
+    username: 'usuario2',
+    password: 'password2',
+  },
+];
 
-app.use((req, res, next) => {
-  validateRequestMethod(req, res, next);
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  
+  const user = users.find((user) => user.username === username && user.password === password);
+
+  if (!user) {
+    return res.status(401).json({ message: 'Credenciales incorrectas' });
+  }
+
+ 
+  const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET, {
+    expiresIn: '1h', 
+  });
+
+  res.json({ token });
 });
 
 
-app.use('/list-view', listViewRouter);
-app.use('/list-edit', listEditRouter);
+app.get('/protegido', (req, res) => {
+  const token = req.headers.authorization;
 
+  if (!token) {
+    return res.status(401).json({ message: 'Token no proporcionado' });
+  }
 
-app.get('/tasks', (req, res) => {
-  res.json(tasks);
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Token inválido' });
+    }
+
+    res.json({ message: 'Ruta protegida', user: decoded.username });
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Servidor en ejecución en http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log('Servidor en funcionamiento en el puerto 3000');
 });
+
+
+
